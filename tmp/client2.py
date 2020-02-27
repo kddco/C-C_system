39e768f731f7f68a48a69bc6a5856d5f1ac96eef
wayne <waynehwang007@gmail.com>
2020-02-27 16:23:38 +0800

    winter vacation(執行子程序 可執行 未仔細檢測)

--- tmp/client2.py
+++ tmp/client2.py
@@ -1,27 +1,32 @@
-# client2.py
-#!/usr/bin/env python
-
+import sys
+import subprocess
+import time
+import os
 import socket
 
 TCP_IP = 'localhost'
-TCP_PORT = 9001
+TCP_PORT = 9177
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-with open('received_file', 'wb') as f:
+with open('dir.exe', 'wb') as f:
     print ('file opened')
     while True:
         #print('receiving data...')
         data = s.recv(BUFFER_SIZE)
-        print('data=%s', (data))
+        print(data)
         if not data:
             f.close()
-            print ('file close()')
             break
         # write data to a file
         f.write(data)
 
 print('Successfully get the file')
-s.close()
 print('connection closed')
+# os.system("pause")
+
+excutionFile = "dir.exe"
+SaveDirectory = os.getcwd() #印出目前工作目錄
+theproc = subprocess.Popen([sys.executable,excutionFile])
+theproc.communicate()
